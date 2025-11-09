dbf757818a25a740a8d54e12c7198fc33d104e4d
karthigaimuthu <122721440+karthigaim070@users.noreply.github.com>
2025-11-09 18:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 const DAY_POWER_MOVE_WEBHOOK = "https://discordapp.com/api/webhooks/1437027104507170937/EnH0fysCXSZUDsh6jPtiWbxFw16cOBZC49T5duOpYVL7W-aoLF_5W8Y92SrdZakciTQn";
 const TODAY_HIGH_LOW_WEBHOOK = "https://discordapp.com/api/webhooks/1436986670754041856/XkMkvyZUpWfv4p6Q2XkCPi15xR5_hBuuvnDuIftdrkBsv_u_5O-UUWxeTqzjD5iC8XeF";
 const POWER_CRT_WEBHOOK = "https://discordapp.com/api/webhooks/1437032285860790354/sbgq_ZA42p1-V8V7YZN2RUmFqYzbBY1NTTb1-1uo8jIl_GV7EaDafdTOwq46_uInl_c5";
+const SETUP_21_WEBHOOK = "https://discordapp.com/api/webhooks/1437061537574555699/F3T-IYk1NTgE1U5DCa9Ej-upnS5ozZybaIPFoouX8WvSWyIMkmJlT2KormwnE1otSRaA";
+
 
 app.post("/", async (req, res) => {
   const alert = req.body;
@@ -30,8 +32,10 @@ app.post("/", async (req, res) => {
     } else if (msg.includes("powercrt")) {
       console.log("powercrt:TRUE");
       webhookUrl = POWER_CRT_WEBHOOK;
-    }
-    else {
+    }else if (msg.includes("2121SETUP")) {
+      console.log("21SETUP:TRUE");
+      webhookUrl = SETUP_21_WEBHOOK;
+    }else {
       console.log("⚠️ Ignored alert — no keyword matched");
       return res.status(200).send("Ignored: no match");
     }
